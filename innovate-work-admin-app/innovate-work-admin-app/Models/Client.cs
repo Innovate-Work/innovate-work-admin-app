1529b6460211eb0fc292b3ac40c896f732ddeefe
Vladislav Zhogol <74291930+Zhigadlo@users.noreply.github.com>
2024-01-18 19:11:05 +0300

    Main models creation

---
+++ innovate-work-admin-app/innovate-work-admin-app/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace innovate_work_admin_app.Models
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime Date { get; set; }
+
+        public bool WithSubscription { get; set; }
+        public bool IsCustom { get; set; }
+    }
+}
---
+++ innovate-work-admin-app/innovate-work-admin-app/Models/JWT/SecurityToken.cs
@@ -0,0 +1,7 @@
+﻿namespace innovate_work_admin_app.Models.JWT
+{
+    public class SecurityToken
+    {
+        public string AccessToken { get; set; }
+    }
+}
